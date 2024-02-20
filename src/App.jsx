d7e02150cc0ad691bcb11500bb9de9a45934935e
Andrii <143963266+andriib77@users.noreply.github.com>
2024-02-20 23:28:32 +0100

    Homework19 commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import "./App.css";
 // Lessons
 // import ProfileCard from "./homeworks/Homework17/ProfileCard";
 // import Lesson18 from "./lessons/Lesson18/Lesson18";
-import Lesson19 from "./lessons/Lesson19/Lesson19";
+// import Lesson19 from "./lessons/Lesson19/Lesson19";
 
 // Homeworks
-import Homework18 from "./homeworks/Homework18/Homework18";
+// import Homework18 from "./homeworks/Homework18/Homework18";
+import Homework19 from "./homeworks/Homework19/Homework19";
 
 // 1. Компоненты пишутся с заглавной буквы
 // 2. Компоненты должны возвращать JSX(DOM элементы - HTML теги)
@@ -19,7 +20,8 @@ function App() {
       {/* <Lesson18 /> */}
       {/* <Lesson19 /> */}
 
-      <Homework18 />
+      {/* <Homework18 /> */}
+      <Homework19 />
     </div>
   );
 }
